8d47e9748593575da04117daae35ab4aad0ed2c2
Facundo Rossi <frossi85@gmail.com>
2020-11-11 15:09:03 -0300

    Initial Parser that parse numeric expresion to be used as a basement for the final parser

---
+++ src/main/scala/example/models/Calculator.scala
@@ -0,0 +1,31 @@
+package example.models
+
+import scala.util.parsing.combinator.PackratParsers
+import scala.util.parsing.combinator.lexical.StdLexical
+import scala.util.parsing.combinator.syntactical.StdTokenParsers
+
+class Calculator extends StdTokenParsers with PackratParsers {
+  override type Tokens = StdLexical
+
+  override val lexical = new StdLexical
+
+  lexical.delimiters ++= List("(", ")", "+", "-", "*", "/")
+
+  lazy val expr: PackratParser[Int] = addSub
+
+  lazy val addSub: PackratParser[Int] = mulDiv * (
+    "+" ^^^ { (left: Int, right: Int) => left + right }
+    | "-" ^^^ { (left: Int, right: Int) => left - right } )
+
+  lazy val mulDiv: PackratParser[Int] = term * (
+    "*" ^^^ { (left: Int, right: Int) => left * right }
+    | "/" ^^^ { (left: Int, right: Int) => left / right } )
+
+  lazy val term: PackratParser[Int] = "(" ~> expr <~ ")" | numericLit ^^ (_.toInt)
+
+  def parse(str: String) = expr(new PackratReader(new lexical.Scanner(str))) match {
+    case Success(result, remain) if remain.atEnd => result
+    case Success(_, remain) => throw new RuntimeException(s"Unparsed input at ${remain.pos}")
+    case NoSuccess(msg, remain) => throw new RuntimeException(s"Parse error $msg at ${remain.pos}")
+  }
+}
